3c02e33f3110899dfd1e51ed6eae33943376071c
Ryan Glazer <rglazer003@gmail.com>
2019-01-24 09:48:26 -0600

    Added makeContacts, modified search to use objects

--- src/contact.java
+++ src/contact.java
@@ -16,43 +16,25 @@ public class contact {
     static Input input = new Input();
     static String directory = "src/data";
     static String filename = "contacts.txt";
+   static List<contact> contactList;
+
+    static {
+        try {
+            contactList = makeContactList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void main(String[] args) {
+
         menu();
     }
 
 
-    public static void showContacts(boolean menu) throws IOException {
-        Path contactsPath = Paths.get(directory, filename);
-        List<String> contacts = Files.readAllLines(contactsPath);
-        List<contact> contactList = new ArrayList<>();
-        String hold1 = "";
-        String hold2 = "";
-        String hold3 = "";
-        int track = 1;
-        int count = 0;
+    public static void showContacts(boolean menu, List<contact> contactList) throws IOException {
         int number = 1;
-        for (String contact : contacts) {
-            if (track == 1) {
-                hold1 = contact;
-                count++;
-            } else if (track == 2) {
-                hold2 = contact;
-                count++;
-            } else if (track == 3) {
-                hold3 = contact;
-                count++;
-            }
-            track++;
-            if (count == 3) {
-                contact entry = new contact(hold1, hold2, hold3);
-                contactList.add(entry);
-                count = 0;
-                track = 1;
-            }
-
-        }
         System.out.println("------ | --------------- | ------------ | --------------");
         System.out.format("%-6s | %-15s | %-5s | %-40s%n", "Index", "Contact Name", "Phone Number", "Email Address");
         System.out.println("------ | --------------- | ------------ | --------------");
@@ -75,7 +57,7 @@ public class contact {
         }
     }
 
-    public contact(String name, String number, String email) {
+    public contact(String name, String number, String email) throws IOException {
         this.name = name;
         this.number = number;
         this.email = email;
@@ -123,23 +105,20 @@ public class contact {
         menu();
     }
 
-    public static void searchContacts() throws IOException {
+    public static void searchContacts(List<contact> contactList) throws IOException {
         String userInput = input.getString("Enter a name to view the phone number for a particular contact:");
         userInput = userInput.toLowerCase();
         Path contactsPath = Paths.get(directory, filename);
-        List<String> contacts = Files.readAllLines(contactsPath);
-        String hold1 = "";
-        String hold2 = "";
-        String hold3 = "";
-        int count = 0;
-        for (String contact : contacts) {
-                if (contact.toLowerCase().contains(userInput)&&!contact.contains("@")) {
-                    hold1 = contact;
-                    hold2 = contacts.get(count + 1);
-                    hold3 = contacts.get(count + 2);
-                    System.out.println(hold1 + " | " + hold2 + " | " + hold3);
+//        List<String> contacts = Files.readAllLines(contactsPath);
+//        String hold1 = "";
+//        String hold2 = "";
+//        String hold3 = "";
+//        int count = 0;
+        for (final contact entry : contactList) {
+                if (entry.name.toLowerCase().contains(userInput)) {
+
+                    System.out.println(entry.name + " | " + entry.number + " | " + entry.email);
                 }
-                count++;
             }
 
         System.out.println();
@@ -148,10 +127,10 @@ public class contact {
         menu();
     }
 
-    public static void deleteContact() throws IOException {
+    public static void deleteContact(List<contact> contactList) throws IOException {
         Path contactsPath = Paths.get(directory, filename);
         List<String> contacts = Files.readAllLines(contactsPath);
-        showContacts(false);
+        showContacts(false, contactList);
         int userInput = input.getInt(1, (contacts.size() / 3), "Enter the Index number of the contact you want to delete: ");
         if (userInput == 1) {
             contacts.remove(userInput - 1);
@@ -167,8 +146,45 @@ public class contact {
 
     }
 
+    public static List<contact> makeContactList () throws IOException {
+        Path contactsPath = Paths.get(directory, filename);
+        List<String> contacts = Files.readAllLines(contactsPath);
+        List<contact> contactList = new ArrayList<>();
+        String hold1 = "";
+        String hold2 = "";
+        String hold3 = "";
+        int track = 1;
+        int count = 0;
+        for (String contact : contacts) {
+            if (track == 1) {
+                hold1 = contact;
+                count++;
+            } else if (track == 2) {
+                hold2 = contact;
+                count++;
+            } else if (track == 3) {
+                hold3 = contact;
+                count++;
+            }
+            track++;
+            if (count == 3) {
+                contact entry = new contact(hold1, hold2, hold3);
+                contactList.add(entry);
+                count = 0;
+                track = 1;
+            }
+
+        }
+        return contactList;
+    }
+
 
     public static void menu() {
+        try { contactList =
+                makeContactList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("Welcome! What would you like to do?");
 
         System.out.println("1: Show all your contacts\n2: Add a new contact\n3: Search a contact by her name\n4: Delete an existing contact\n0: Exit");
@@ -178,7 +194,7 @@ public class contact {
 
         if (userChoice == 1) {
             try {
-                showContacts(true);
+                showContacts(true, contactList);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -190,16 +206,17 @@ public class contact {
             }
         } else if (userChoice == 3) {
             try {
-                searchContacts();
+                searchContacts(contactList);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (userChoice == 4) {
             try {
-                deleteContact();
+                deleteContact(contactList);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
 
         } else if (userChoice == 0) {
             System.out.println("Goodbye");
